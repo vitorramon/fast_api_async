a4b8af89ec8939408894a3dbc1d41a59b91710b4
Vitor Ramon <vitorramon.info@gmail.com>
2025-06-22 19:02:19 -0400

    Estudo de Status Code - Requisições HTTP

--- fast_api_async/app.py
+++ fast_api_async/app.py
@@ -1,8 +1,33 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+from fast_api_async.schemas import Message
+
+app = FastAPI(title='Minha API')
 
 
-@app.get('/')
+@app.get('/',
+         status_code=HTTPStatus.OK,
+         response_model=Message
+         )
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.get('/pagina-test',
+         status_code=HTTPStatus.OK,
+         response_class=HTMLResponse
+         )
+def read_hello():
+    return """
+    <html>
+        <head>
+            <title>FastAPI Async Example</title>
+        </head>
+        <body>
+            <h1>Olá mundo!</h1>
+        </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -21,3 +21,22 @@ def test_root_deve_retornar_ola_mundo():
     # Assert
     assert response.json() == {'message': 'Olá mundo!'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_pagina_test_deve_retornar_html():
+    """
+    Esse teste tem 3 etapas (AAA)
+
+    - A: Arrange - Arranjo
+    - A: Act - Executa o System Under Test (SUT)
+    - A: Assert - Garanta que A == A
+
+    """
+    # Arrange
+    client = TestClient(app)
+    # Act
+    response = client.get('/pagina-test')
+    # Assert
+    assert response.status_code == HTTPStatus.OK
+    assert response.headers['content-type'] == 'text/html; charset=utf-8'
+    assert '<h1>Olá mundo!</h1>' in response.text
