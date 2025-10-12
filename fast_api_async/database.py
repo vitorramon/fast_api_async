6ed09b979ee86685ed54a01f659e83f8045b2a08
Vitor Ramon <vitorramon.info@gmail.com>
2025-10-12 18:06:45 -0400

    feat: adicionar configuração de banco de dados
    - Criar módulo database.py com função get_session
    - Corrigir formatação em settings.py para consistência

---
+++ fast_api_async/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from fast_api_async.settings import Settings
+
+engine = create_engine(Settings().DATABASE_URL)
+
+
+def get_session():
+    """
+    Returns a new SQLAlchemy session.
+    """
+    with Session(engine) as session:
+        yield session
